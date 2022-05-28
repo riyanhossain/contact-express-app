4f981343cc415f4944a653b0376e19567f12a35c
mehrabriyan <mehraabriyan@gmail.com>
2022-05-28 22:24:43 +0600

    update api done

--- Routes/contactRoute.js
+++ Routes/contactRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../Middleware/uploadMiddleware");
-const { postContacts, getContacts, deleteContacts } = require("../Controllers/contacts");
+const { postContacts, getContacts, deleteContacts, updateContacts } = require("../Controllers/contacts");
 
 router.post("/contacts", upload.single("avatar"), postContacts);
 
 router.get("/contacts", getContacts);
 
 router.delete("/contacts/delete/:id", deleteContacts);
+
+router.put("/contacts/update/:id", upload.single("avatar"), updateContacts);
 
 module.exports = router;
--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,88 +1,109 @@
 const contact = require("../Models/contacts");
 const fs = require("fs");
 
-const postContacts =  async (req, res) => {
-    const newUrl = new URL(`${req.protocol}://${req.get('host')}`);
-    try {
-        const newContact = new contact({
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
-            location : req.body.location,
-            occupation : req.body.occupation,
-            permission : req.body.permission,
-            bloodGroup : req.body.bloodGroup,
-            birthDate : req.body.birthDate,
-            fbIdLink : req.body.fbIdLink,
-            avatar : newUrl.origin +'/uploads/' + req.file.filename,
-            fileName : req.file.filename
-        });
-        await newContact.save();
-        res.status(200).json({
-            message: 'Contact added successfully',
-        });
-    } catch (err) {
-        res.status(500).json({
-            message: err.message
-        });
-    }
-}
+const postContacts = async (req, res) => {
+  const newUrl = new URL(`${req.protocol}://${req.get("host")}`);
+  try {
+    const newContact = new contact({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      location: req.body.location,
+      occupation: req.body.occupation,
+      permission: req.body.permission,
+      bloodGroup: req.body.bloodGroup,
+      birthDate: req.body.birthDate,
+      fbIdLink: req.body.fbIdLink,
+      avatar: newUrl.origin + "/uploads/" + req.file.filename,
+      fileName: req.file.filename,
+    });
+    await newContact.save();
+    res.status(200).json({
+      message: "Contact added successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
 
 const getContacts = async (req, res) => {
-    const Offset = isNaN(req.query.page)
-      ? 1
-      : parseInt(req.query.page) <= 1
-      ? 1
-      : parseInt(req.query.page);
-    try {
-      const contacts = await contact.paginate(
-        {},
-        { offset: 8 * (Offset - 1), limit: 8 }
-      );
-      if (contacts.totalPages >= Offset) {
-        res.status(200).json({
-          message: "Contacts fetched successfully",
-          contacts: contacts.docs,
-          nextPage: Offset + 1,
-          prevPage: Offset - 1,
-          totalPages: contacts.totalPages,
-        });
-      } else {
-        res.status(200).json({
-          message: "constacts not found",
-          prevPage: Offset - 1,
-        });
-      }
-    } catch {
-      (err) => {
-        res.status(500).json({
-          message: err.message,
-        });
-      };
+  const Offset = isNaN(req.query.page)
+    ? 1
+    : parseInt(req.query.page) <= 1
+    ? 1
+    : parseInt(req.query.page);
+  try {
+    const contacts = await contact.paginate(
+      {},
+      { offset: 8 * (Offset - 1), limit: 8 }
+    );
+    if (contacts.totalPages >= Offset) {
+      res.status(200).json({
+        message: "Contacts fetched successfully",
+        contacts: contacts.docs,
+        nextPage: Offset + 1,
+        prevPage: Offset - 1,
+        totalPages: contacts.totalPages,
+      });
+    } else {
+      res.status(200).json({
+        message: "constacts not found",
+        prevPage: Offset - 1,
+      });
     }
-  }
-
-  const deleteContacts = async (req, res) => {
-    try {
-      const contacts = await contact.findByIdAndDelete(req.params.id);
-      if (!contacts) {
-       
-        res.status(404).json({
-          message: "Contact not found",
-          
-        });
-
-      } else {
-        fs.unlinkSync(`public/uploads/${contacts.fileName}`);
-        res.status(200).json({
-          message: "Contact deleted successfully",
-        });
-      }
-    } catch (err) {
+  } catch {
+    (err) => {
       res.status(500).json({
         message: err.message,
       });
-    }
+    };
   }
+};
 
-module.exports = { postContacts , getContacts, deleteContacts};
+const deleteContacts = async (req, res) => {
+  try {
+    const contacts = await contact.findByIdAndDelete(req.params.id);
+    if (!contacts) {
+      res.status(404).json({
+        message: "Contact not found",
+      });
+    } else {
+      fs.unlinkSync(`public/uploads/${contacts.fileName}`);
+      res.status(200).json({
+        message: "Contact deleted successfully",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const updateContacts = async (req, res) => {
+  const newUrl = new URL(`${req.protocol}://${req.get("host")}`);
+  console.log(req.file, req.body, newUrl);
+  try {
+    req.body.avatar = newUrl.origin + "/uploads/" + req.file.filename;
+    req.body.fileName = req.file.filename;
+    const contacts = await contact.findByIdAndUpdate(req.params.id, req.body);
+    if (!contacts) {
+      res.status(404).json({
+        message: "Contact not found",
+      });
+    } else {
+      fs.unlinkSync(`public/uploads/${contacts.fileName}`);
+      res.status(200).json({
+        message: "Contact updated successfully",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports = { postContacts, getContacts, deleteContacts, updateContacts };
